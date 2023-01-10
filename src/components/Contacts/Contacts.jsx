1b079eec8f95677b9334c9df6d7c523659c44c54
Juliya Boiko <boikojuliya2707@gmail.com>
2023-01-10 21:56:55 +0200

    add Notiflix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,6 +1,7 @@
 import { Formik, ErrorMessage } from "formik";
 import * as yup from 'yup';
 import { ContactsContent, ContactsForm, ContactsBtn, ContactsInput, ContactsError } from "./Contacts.styled";
+import Notiflix from 'notiflix';
 require("yup-phone");
 
 const nameRegExp = RegExp(
@@ -10,18 +11,18 @@ const nameRegExp = RegExp(
 export const contactSchema = yup.object().shape({
   name: yup.string().matches(nameRegExp).min(2).required(),
   number: yup.string().phone().required(),
-  // number: yup.string()
-  //   .matches(
-  //       /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-  //       "Phone number is not valid")
-  //   .required(),
+});
+
+Notiflix.Notify.init({
+  position: 'center-top',
+  closeButton: false,
 });
 
 export const Contacts = () => {
 
   const submitHandler = async (values) => {
     await new Promise((r) => setTimeout(r, 500));
-    alert(JSON.stringify(values, null, 2));
+    Notiflix.Notify.success(`Dear ${values.name}, we will contact you soon`);
   };
 
   return (
@@ -31,7 +32,6 @@ export const Contacts = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.3816716564615!2d30.620339815776624!3d50.43399107947318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c569ae705387%3A0x63844605be723e7!2z0KDQtdCz0LXQvdC10YDQsNGC0L7RgNC90LDRjyA0LCDQmtC-0YDQv9GD0YEgNywgNCwg0JrQuNC10LIsINCj0LrRgNCw0LjQvdCwLCAwMjAwMA!5e0!3m2!1sru!2sbg!4v1673373097298!5m2!1sru!2sbg"
         width="648"
         height="355"
-       // style={"border:0;"}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"></iframe>
